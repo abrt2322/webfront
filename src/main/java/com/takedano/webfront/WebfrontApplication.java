4380f58fcc0e9b9db9cdfed3dac0736953d5a7f2
abrt2322 <rtsoccer048@gmail.com>
2020-07-20 16:54:25 +0900

    Initial commit

---
+++ src/main/java/com/takedano/webfront/WebfrontApplication.java
@@ -0,0 +1,13 @@
+package com.takedano.webfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfrontApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/takedano/webfront/WebfrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takedano.webfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
